5db07df4da812c5b095f62f0cc2b3cfeaa218297
Erwin Pranata <erwin.pranata@admedika.co.id>
2019-12-12 14:21:38 +0700

    'first'

---
+++ Style.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { 
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native';
+
+import Stylex from "./style/styleComponent";
+
+export default class Style extends Component{
+    render() {
+        return(
+            <View style={ Stylex.container }>
+                <Text style={ Stylex.textRed }>RN Style</Text>
+            </View>
+        )
+    }
+}
+
+// STYLE
+const styles = StyleSheet.create({
+
+    container : {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textRed : {
+        color: "red",
+        fontSize: 40
+    }
+
+})
